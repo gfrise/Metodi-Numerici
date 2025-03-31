23f3524f645a9e686066c2e5f47e5c7d6d7e6116
Giuseppe Frisella <104461249+gfrise@users.noreply.github.com>
2025-03-31 23:53:23 +0200

    Update uniforme_a_poisson.c

--- uniforme_a_poisson.c
+++ uniforme_a_poisson.c
@@ -1,23 +1,18 @@
 #include<stdio.h>
+#include<math.h>
 
-int poissonRandom(double mu){
-    double u = (double)rand()/RAND_MAX;
-    double prob = exp(-mu);
-    double cumulative = prob;
-    int n = 0;
-
-    while (u>cumulative){
-        n++;
-        prob*=mu/n;
-        cumulative+=prob;
+int poisson(double mu){
+    double u=rand()/(double)RAND_MAX;
+    double pb=exp(-mu),acc=p;
+    int n =0;
+    while(u>acc){
+        pb*=mu/++n;
+        acc+=pb;
     }
     return n;
 }
 
 int main(){
-    double mu = 3;
-    int i,n=10;
-    for(i=0;i<n;i++){
-        printf("evento %d:%d\n",i+1,poissonRandom(mu));
-    }
+    for(int i=0;i<10;i++)
+    printf("evento %d: %d\n",i+1,poisson(3));
 }
