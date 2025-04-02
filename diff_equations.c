1701e366bcb9fe16c40c965c07639899878acb87
Giuseppe Frisella <104461249+gfrise@users.noreply.github.com>
2025-04-02 15:36:58 +0200

    Update diff_equations.c

--- diff_equations.c
+++ diff_equations.c
@@ -1,54 +1,63 @@
 #include<stdio.h>
 #include<math.h>
 
-#define H 0.000005
-#define TF 5.5
-#define EPS 1e-6
-#define N ((int)(TF/H))
-
-double f(double t,double x) {
-    return cos(5*t)+x;
+double f(double x,double y){
+    return cos(5*x)+y;
 }
 
-double taylor(double t0,double x0,int i){
-    double h = H;
-    double f0 = f(t0,x0);
-    double f1 = -5*sin(5*t0)+f0;
-    double f2 = -25*cos(5*t0)+f1;
-    double f3 = 125*sin(5*t0)+f2;
-    double f4 = 625*cos(5*t0)+f3;
-    return x0+i*h*f0+pow(i*h,2)*f1/2+pow(i*h,3)*f2/6+pow(i*h,4)*f3/24+pow(i*h,5)*f4/120;
+double taylor(double x,double y,int i,double h){
+    double dx=i*h, f0=f(x,y);
+    double f1=-5*sin(5*x)+f0;
+    double f2=-25*cos(5*x)+f1;
+    double f3=125*sin(5*x)+f2;
+    double f4=625*cos(5*x)+f3;
+    return y+dx*f0+pow(dx,2)*f1/2+pow(dx,3)*f2/6+pow(dx,4)*f3/24+pow(dx,5)*f4/120;
 }
 
-void initialize(double t[],double x[]){
-    for(int i=0;i<4;i++){
-        t[i] = i*H;
-        x[i] = taylor(0,0,i);
+void start(double x[],double y[],double x0,double y0,double h){
+    x[0]=x0; y[0]=y0;
+    for(int i=1;i<4;i++){
+        x[i]=x0+i*h;
+        y[i]=taylor(x0,y0,i,h);
+        printf("%d\t%f\t%f\n",i,x[i],y[i]);
     }
 }
 
-void predictor_corrector(double t[],double x[]){
-    for (int i=4;i<= N;i++){
-        t[4]=t[3]+H;
-        x[4]=x[3]+H*(55*f(t[3],x[3])-59*f(t[2],x[2])+37*f(t[1],x[1])-9*f(t[0],x[0]))/24;
-
-        double x_new;
-        do {
-            x_new = x[4];
-            x[4]=x[3]+H*(9*f(t[4],x_new)+19*f(t[3],x[3])-5*f(t[2],x[2])+f(t[1],x[1]))/24;
-        } while (fabs(x[4]-x_new)>EPS);
-        
-        printf("t = %.2lf, x = %.6lf\n",t[4],x[4]);
-
-        for (int j=0;j<4;j++){
-            t[j]=t[j+1];
+double ab(double x0,double y0,double h,double xf,double eps){
+    int n=(int)((xf-x0)/h);
+    double x[5],y[5];
+    start(x,y,x0,y0,h);
+    for(int i=4;i<=n;i++){
+        x[4]=x[3]+h;
+        y[4]=y[3]+h*(55*f(x[3],y[3])-59*f(x[2],y[2])
+        +37*f(x[1],y[1])-9*f(x[0],y[0]))/24;
+        double err=1;
+        while(err>eps){
+            double ws=y[3]+h*(9*f(x[4],y[4])+19*f(x[3],y[3])
+                 -5*f(x[2],y[2])+f(x[1],y[1]))/24;
+            err=fabs(y[4]-ws);
+            y[4]=ws;
+        }
+        printf("%d\t%f\t%f\n",i,x[4],y[4]);
+        for(int j=0;j<4;j++){
             x[j]=x[j+1];
+            y[j]=y[j+1];
         }
     }
+    return y[3];
 }
 
 int main(){
-    double t[5],x[5];
-    initialize(t,x);
-    predictor_corrector(t,x);
+    double x0=0,y0=0,xf=3.5,h=0.05,eps=1e-2,s1,s2,s3;
+    printf("h = %f, eps = %f\n", h, eps);
+    s1 = ab(x0, y0, h, xf, eps);
+    eps /= 10;
+    printf("\nNuovo eps: %le\n", eps);
+    printf("\nCon h = %.2lf, eps = %le\n", h, eps);
+    s2 = ab(x0, y0, h, xf, eps);
+    printf("\nDifferenza (stesso h, eps differente): %le\n", fabs(s2 - s1));
+    h = 0.25;
+    printf("\nCon h = %.2lf, eps = %le\n", h, eps);
+    s3 = ab(x0, y0, h, xf, eps);
+    printf("\nDifferenza (stesso eps, h differente): %le\n", fabs(s3 - s2));
 }
